6b7da8bc6cc4bd1b21ad1fb807846df72d9e5376
Jonathan Ströbele <mail@jonathanstroebele.de>
2024-05-23 17:18:15 +0200

    feat: download healthsites.io data

--- src/datalayer/healthsitesio_facilities.py
+++ src/datalayer/healthsitesio_facilities.py
@@ -1,5 +1,6 @@
 import re
 import os
+import subprocess
 
 import geopandas
 from shapely import wkt
@@ -20,6 +21,22 @@ class HealthsitesioFacilities(BaseLayer):
         self.raw_vector_data_table = 'data_healthsitesio_facilities'
 
     def download(self):
+        url = "https://healthsites.io/api/public/facilities/shapefile/Ghana/download"
+        file_name = 'Ghana.zip'
+
+        if not os.path.isfile(self.get_data_path() / file_name):
+            self._save_url_to_file(url, folder=self.get_data_path(), file_name=file_name)
+
+        if os.path.isfile(self.get_data_path() / "LICENSE.txt"):
+            return
+        try:
+            in_file = self.get_data_path() / file_name
+            out_dir = self.get_data_path().as_posix()
+            subprocess.run(f'unzip {in_file} -d {out_dir}', shell=True,
+                capture_output=True, check=True)
+        except subprocess.CalledProcessError as error:
+            self.layer.warning("Could not unzip files: %s", error.stderr)
+
         gdf = geopandas.read_file(self.get_data_path() / 'Ghana-node.shp')
         gdf.to_postgis(self.raw_vector_data_table, con=get_engine(), if_exists='replace')
 
