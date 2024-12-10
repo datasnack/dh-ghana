e3751c8bf17e86ff21138b76b842f67a77d9c401
Jonathan Ströbele <mail@jonathanstroebele.de>
2024-12-10 12:19:31 +0100

    new DHS layers

---
+++ src/datalayer/dhs_media_female.py
@@ -0,0 +1,40 @@
+from datalayers.datasources.dhs_layer import DhsLayer
+from datalayers.datasources.base_layer import LayerValueType
+
+
+class DhsMediaFemale(DhsLayer):
+    def __init__(self) -> None:
+        super().__init__()
+        self.value_type = LayerValueType.PERCENTAGE
+
+    def get_indicators(self) -> list[str]:
+        return ["ED_MDIA_W_3MD"]
+
+    def get_countries(self) -> list[str]:
+        return ["GH"]
+
+    def _mapping(self):
+        """
+        In 2018 Ghana created 6 new regions bei splitting up previous regions.
+
+        Northern ->
+            Northern
+            North East
+            Savannah
+
+        Volta ->
+            Volta
+            Oti
+
+        Western ->
+            Western
+            Western North
+
+        Brong-Ahafo ->
+            Bono
+            Ahafo
+            Bono East.
+
+
+        """
+        pass
