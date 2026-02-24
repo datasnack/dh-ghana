0f255821d5b5fc760bd4cecd7956d1246d02ad3c
Jonathan Ströbele <mail@jonathanstroebele.de>
2026-02-24 11:41:06 +0100

    feat: add fao sources

---
+++ src/datalayer/fao_livestock_goat.py
@@ -0,0 +1,30 @@
+import os
+import re
+
+import numpy as np
+from datalayers.datasources.base_layer import (
+    BaseLayer,
+    LayerTimeResolution,
+    LayerValueType,
+)
+from datalayers.datasources.tiff_layer import TiffLayer
+
+
+class FaoLivestockGoat(TiffLayer):
+    def __init__(self) -> None:
+        super().__init__()
+        self.value_type: LayerValueType = LayerValueType.FLOAT
+
+    def download(self):
+        urls = [
+            "https://storage.googleapis.com/fao-gismgr-glw4-2020-data/DATA/GLW4-2020/MAPSET/D-DA/GLW4-2020.D-DA.GTS.tif"
+        ]
+
+        for url in urls:
+            self._save_url_to_file(url)
+
+    def consume(self, file, band, shape):
+        x = re.search(r"[0-9]{4}", os.path.basename(file))
+        year = int(x[0])
+
+        self.add_value(shape, year, np.nanmean(band))
